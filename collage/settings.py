fa8467143f555216cd45168697a4a47d78054a07
iyerikuzwe <iyerikuzweregine19@gmail.com>
2019-03-22 10:55:39 +0200

    deploy to heroku

--- collage/settings.py
+++ collage/settings.py
@@ -22,22 +22,22 @@ MODE=config("MODE", default="dev")
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
-development
-if config('MODE')=="dev":
-   DATABASES = {
-       'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': '',
+# development
+# if config('MODE')=="dev":
+#    DATABASES = {
+#        'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': config('DB_NAME'),
+#         'USER': config('DB_USER'),
+#         'PASSWORD': config('DB_PASSWORD'),
+#         'HOST': config('DB_HOST'),
+#         'PORT': '',
 #         # 'NAME': 'collage',
 #         # 'USER': 'wecode',
 #         # 'PASSWORD':'regine1990',
-        }
+    #     }
        
-    }
+    # }
 
 DATABASES = {
         'default': dj_database_url.config(
